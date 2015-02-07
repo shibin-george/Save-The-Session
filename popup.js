a35868f7c2955d4e192f859bc6ef2bec07f023c2
Shibin George <george.shibin1993@gmail.com>
2015-02-07 17:14:36 +0530

    Key-value pairs successfully written to chrome.storage.local.

--- popup.js
+++ popup.js
@@ -4,74 +4,74 @@
    *         National Institute of Technology, Warangal
    **/
 
-   function getData(callback, errorCallback) {
+function getData(callback, errorCallback) {
 
-    //code to query the tabs
-    var s = "";
-    //var padding = "               ";
+  //code to query the tabs
+  var s = "";
+  //var padding = "               ";
 
-    chrome.tabs.query({'currentWindow':true}, function(tabs){
-      var checkList = document.createElement("div");
-      checkList.setAttribute("id", "checklist");
-      document.getElementById('status').innerHTML = 'The open tabs are: \n';
-      document.getElementById('status').appendChild(checkList);
+  chrome.tabs.query({'currentWindow':true}, function(tabs){
+    var checkList = document.createElement("div");
+    checkList.setAttribute("id", "checklist");
+    document.getElementById('status').innerHTML = 'The open tabs are: \n';
+    document.getElementById('status').appendChild(checkList);
 
-      //code for appropriate padding
-      var maxlen = 0;
-      for(i=0;i<tabs.length;i++){
-        s = tabs[i].url;
-        maxlen = Math.max(maxlen, s.length);
-      }
-      var displayLength = maxlen - 3*maxlen/4;
+    //code for appropriate padding
+    var maxlen = 0;
+    for(i=0;i<tabs.length;i++){
+      s = tabs[i].url;
+      maxlen = Math.max(maxlen, s.length);
+    }
+    var displayLength = maxlen - 3*maxlen/4;
 
-      if(displayLength < 40)
-        displayLength = 40;
-      if(displayLength > 60)
-        displayLength = 40;
-      console.log(maxlen + ' \tdisplayLength = ', displayLength);
+    if(displayLength < 40)
+      displayLength = 40;
+    if(displayLength > 60)
+      displayLength = 40;
+    console.log(maxlen + ' \tdisplayLength = ', displayLength);
 
-      for(i=0;i<tabs.length;i++){
-        var s = tabs[i].url;
-        var shortname=returnShortName(s, displayLength);
-console.log(shortname);
-        //add a checkbox by the tab's name
-        var box = document.createElement("div");
-        box.setAttribute("id", "Checkbox" + i);
-        box.setAttribute("name", s);
-
-        //TODO: this here is a vulnerability.
-        //if two tabs with the exact same url are open, 
-        //this may show undefined behaviour.
-        //As per HTML standard, 
-        //no two elements should have same ID, document-wide.
-        box.innerHTML = '<input type="checkbox" name="Select" id=' + s +'>\t' + shortname;
-
-        //add the checkbox to the list
-        document.getElementById('checklist').appendChild(box);      
-      }
-
-      //add a checkbox to select all
+    for(i=0;i<tabs.length;i++){
+      var s = tabs[i].url;
+      var shortname=returnShortName(s, displayLength);
+      console.log(shortname);
+      //add a checkbox by the tab's name
       var box = document.createElement("div");
-      box.setAttribute("id", "CheckAll");
-      box.innerHTML = '\n<input type="checkbox" id="SelectALL">\t' + 'Click Here to select All\n';
-      document.getElementById('checklist').appendChild(box);
+      box.setAttribute("id", "Checkbox" + i);
+      box.setAttribute("name", s);
 
-      //add an event handler for the 'Select all' checkbox
-      //this invokes the toggle() function
-      document.getElementById('SelectALL').onchange = function(){
-        toggle();
-      };
+      //TODO: this here is a vulnerability.
+      //if two tabs with the exact same url are open, 
+      //this may show undefined behaviour.
+      //As per HTML standard, 
+      //no two elements should have same ID, document-wide.
+      box.innerHTML = '<input type="checkbox" name="Select" id=' + s +'>\t' + shortname;
 
-      //add a button to finalise the list
-      var button = document.createElement("div");
-      button.setAttribute("id", "OKButton");
-      button.innerHTML = '\n<button> Click to add </button>';
-      button.onclick = function(){
-        finalizeChoice();
-      }
-      
-      document.getElementById('checklist').appendChild(button);
-    });
+      //add the checkbox to the list
+      document.getElementById('checklist').appendChild(box);      
+    }
+
+    //add a checkbox to select all
+    var box = document.createElement("div");
+    box.setAttribute("id", "CheckAll");
+    box.innerHTML = '\n<input type="checkbox" id="SelectALL">\t' + 'Click Here to select All\n';
+    document.getElementById('checklist').appendChild(box);
+
+    //add an event handler for the 'Select all' checkbox
+    //this invokes the toggle() function
+    document.getElementById('SelectALL').onchange = function(){
+      toggle();
+    };
+
+    //add a button to finalise the list
+    var button = document.createElement("div");
+    button.setAttribute("id", "OKButton");
+    button.innerHTML = '\n<button> Click to add </button>';
+    button.onclick = function(){
+      finalizeChoice();
+    }
+    
+    document.getElementById('checklist').appendChild(button);
+  });
 }
 
 function returnShortName(s, d){
@@ -96,54 +96,77 @@ function toggle(){
         checks[i].checked = bool;
       }
     }
-  }
+}
 
-  function finalizeChoice(){
-    // TODO: to be fixed as soon as
-    // a workaround to the previous TODO is found.
-    var tabs = document.getElementsByName('Select');
-    var s = "";
-    var numChoices = 0;
-    console.log("Number of checkboxes = " + tabs.length);
-    for(i=0; i<tabs.length; i++){
-      if(tabs[i].checked==true){
-        //this particular tab has to be added
-        s += returnShortName(tabs[i].id, 40) + "\n";
-        numChoices += 1;
-      }
+function finalizeChoice(){
+  // TODO: to be fixed as soon as
+  // a workaround to the previous TODO is found.
+  var tabs = document.getElementsByName('Select');
+  var s = "";
+  var numChoices = 0;
+  console.log("Number of checkboxes = " + tabs.length);
+  for(i=0; i<tabs.length; i++){
+    if(tabs[i].checked==true){
+      //this particular tab has to be added
+      s += returnShortName(tabs[i].id, 40) + "\n";
+      numChoices += 1;
     }
-    //renderStatus("You have chosen the following tabs:\n" + s);
-    document.getElementById('status').innerHTML = '';
-    var tnode;
-    var status = document.getElementById('status');
-    //renderStatus('');
-    if(numChoices > 0){      
-      var rand = Math.floor(Math.random()*1100);
-      var inputHTML = '\nEnter the profile name: <input type="text" name="ProfileName" value="Profile' + rand + '"><br>';
-      var buttonHTML = ''
-      status.innerHTML = 'You have chosen the following tabs:\n' + s + inputHTML ;
+  }
+  //renderStatus("You have chosen the following tabs:\n" + s);
+  document.getElementById('status').innerHTML = '';
+  var tnode;
+  var status = document.getElementById('status');
+  //renderStatus('');
+  if(numChoices > 0){      
+    var rand = Math.floor(Math.random()*101010);
+    var inputHTML = '\nEnter the profile name: <input type="text" id="ProfileName" value="Profile' + rand + '"><br>';
+    var buttonHTML = ''
+    status.innerHTML = 'You have chosen the following tabs:\n\n' + s + inputHTML ;
+    
+    var button = document.createElement("div");
+    button.setAttribute("id", "ProfileSubmitButton");
+    var b = '\n<button> Click to save </button>';
+    b += '\n\n** NOTE: The profile name above is randomly generated.\
+    \nIt is highly recommended that you change the name\
+    \nto something not used before. Otherwise, this may\
+    \nlead to the profile being overwritten. Keep it short and simple'
+    button.innerHTML = b; 
+    button.style.fontSize = "smaller";
+    button.style.color = "red";
+    status.appendChild(button);
+
+    button.onclick = function(){
+      var key = document.getElementById("ProfileName").value;
+      var obj = {};
+      obj[key] = s;
+      chrome.storage.local.set(obj);
+      //chrome.
+      renderStatus('Profile saved to\n' + key);
       
-      var button = document.createElement("div");
-      button.setAttribute("id", "ProfileSubmitButton");
-      button.innerHTML = '\n<button> Click to save </button>';
-      status.appendChild(button);
-      button.onclick = function(){
-        renderStatus('You are awesome Shibin.\n');
-        //finalizeChoice();
-      }
-    } else {
-      status.innerHTML = "You must choose atleat one tab."
-    }    
-  }
+      //write the profile to the local storage
+      //var 
+      debug();
+    }
+  } else {
+    status.innerHTML = "You must choose atleat one tab."
+  }    
+}
 
-  function renderStatus(statusText) {
-    document.getElementById('status').textContent = statusText;
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {  
-    renderStatus('Loading the content...');
-    getData({
-    }, function(errorMessage) {
-      renderStatus('Cannot display results: ' + errorMessage);
-    });
+function debug(){
+  chrome.storage.local.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    console.log(allKeys);
   });
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;4
+}
+
+document.addEventListener('DOMContentLoaded', function() {  
+  renderStatus('Loading the content...');
+  getData({
+  }, function(errorMessage) {
+    renderStatus('Cannot display results: ' + errorMessage);
+  });
+});
